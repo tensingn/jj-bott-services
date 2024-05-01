795a02ef1e1cff772ac093cc6abd4efd347bb475
tensingn <tensingn@oregonstate.edu>
2024-05-01 05:42:59 -0400

    adding get all players to sleeper service

--- package/src/sleeper/sleeper.service.ts
+++ package/src/sleeper/sleeper.service.ts
@@ -1,13 +1,18 @@
 import axios from "axios";
 import { NFLStateSleeperModel } from "./models/nfl-state.sleeper.model";
+import { PlayersSleeperModel } from "@tensingn/son-of-botker-models";
 
 export class SleeperService {
 	private readonly url = "https://api.sleeper.app/v1";
 
 	constructor() {}
 
-	async getNFLState(): Promise<NFLStateSleeperModel> {
+	getNFLState(): Promise<NFLStateSleeperModel> {
 		return this.get<NFLStateSleeperModel>("state/nfl");
+	}
+
+	getAllPlayers(): Promise<PlayersSleeperModel> {
+		return this.get<PlayersSleeperModel>("players/nfl");
 	}
 
 	private async get<TResponse>(

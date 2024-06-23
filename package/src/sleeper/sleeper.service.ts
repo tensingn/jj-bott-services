a9015e125aef57529ee27620980fcc8fe94ed22f
tensingn <tensingn@oregonstate.edu>
2024-06-22 22:33:56 -0400

    adding some sleeper methods

--- package/src/sleeper/sleeper.service.ts
+++ package/src/sleeper/sleeper.service.ts
@@ -1,6 +1,12 @@
 import axios from "axios";
 import { NFLStateSleeperModel } from "./models/nfl-state.sleeper.model";
-import { PlayersSleeperModel } from "@tensingn/jj-bott-models";
+import {
+	MatchupSleeperModel,
+	PlayersSleeperModel,
+	RosterSleeperModel,
+	SeasonSleeperModel,
+	UserSleeperModel,
+} from "@tensingn/jj-bott-models";
 
 export class SleeperService {
 	private readonly url = "https://api.sleeper.app/v1";
@@ -15,10 +21,23 @@ export class SleeperService {
 		return this.get<PlayersSleeperModel>("players/nfl");
 	}
 
-	private async get<TResponse>(
-		uri: string,
-		params: {} = {}
-	): Promise<TResponse> {
+	getSeason(seasonID: string): Promise<SeasonSleeperModel> {
+		return this.get<SeasonSleeperModel>(`league/${seasonID}`);
+	}
+
+	getAllRostersForSeason(seasonID: string): Promise<Array<RosterSleeperModel>> {
+		return this.get<Array<RosterSleeperModel>>(`league/${seasonID}/rosters`);
+	}
+
+	getAllMatchupsForWeek(seasonID: string, week: string): Promise<Array<MatchupSleeperModel>> {
+		return this.get<Array<MatchupSleeperModel>>(`league/${seasonID}/matchups/${week}`);
+	}
+
+	getAllUsersForSeason(seasonID: string): Promise<Array<UserSleeperModel>> {
+		return this.get<Array<UserSleeperModel>>(`league/${seasonID}/users`);
+	}
+
+	private async get<TResponse>(uri: string, params: {} = {}): Promise<TResponse> {
 		const res = await axios.get<TResponse>(`${this.url}/${uri}`, {
 			params,
 		});
